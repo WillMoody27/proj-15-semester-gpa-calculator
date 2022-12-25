99b0d4d64f6e6eec654c98a9df6e48c976ea7ec0
William Hellems-Moody <94521548+WillMoody27@users.noreply.github.com>
2022-12-25 06:05:15 -0700

    calculator css html and JS update

--- darkmode.js
+++ darkmode.js
@@ -1,32 +1,47 @@
-const toggleBtn = document.querySelector(".btn");
+const darkModeSun = document.querySelector(".fa-sun");
+const darkModeMoon = document.querySelector(".fa-moon");
 const docBody = document.documentElement;
 const darkModeFeature = localStorage.getItem("isDarkMode");
 
-// Dark Mode Feature
 if (darkModeFeature === "enabled") {
-  darkModeOn(); // Initial State of Dark Mode
+  darkModeOn();
 }
+
 // Mouseover checks theme status & if darkmode in localStorage
-toggleBtn.addEventListener("mouseover", () => {
-  if (darkModeFeature == null && darkModeFeature === "disabled") {
+darkModeSun.addEventListener("mouseover", () => {
+  console.log("MOuse");
+  if (darkModeFeature == null || darkModeFeature === "disabled") {
     localStorage.setItem("isDarkMode", "disabled");
   }
 });
-toggleBtn.addEventListener("click", () => {
-  if (localStorage.getItem("isDarkMode") === "disabled") {
+
+// SHOW MOON
+darkModeSun.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("hide")) {
+    e.target.classList.add("hide");
+    darkModeMoon.classList.remove("hide");
     darkModeOn();
-  } else {
+  }
+});
+// SHOW SUN
+darkModeMoon.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("hide")) {
+    e.target.classList.add("hide");
+    darkModeSun.classList.remove("hide");
     darkModeOff();
   }
 });
-// ***** Functions *****
+
+// DARK MODE ON/OFF
 function darkModeOn() {
   docBody.classList.add("dark-mode");
-  toggleBtn.classList.add("dark-btn");
+  darkModeSun.classList.add("hide");
+  darkModeMoon.classList.remove("hide");
   localStorage.setItem("isDarkMode", "enabled");
 }
 function darkModeOff() {
   docBody.classList.remove("dark-mode");
-  toggleBtn.classList.remove("dark-btn");
+  darkModeSun.classList.remove("hide");
+  darkModeMoon.classList.add("hide");
   localStorage.setItem("isDarkMode", "disabled");
 }
